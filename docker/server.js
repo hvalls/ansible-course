2cf94e796b846e425c0c0d768bd530a6b505e273
Héctor Valls Mondragón <hvalls198@gmail.com>
2023-03-16 15:12:26 +0100

    exit signal support

--- docker/server.js
+++ docker/server.js
@@ -1,7 +1,11 @@
 const http = require("http");
 
+process.on('SIGINT', function() {
+    process.exit();
+});
+
 const server = http.createServer((req, res) => {
   res.end("Hello from the server");
 });
 
-server.listen(8080);
+server.listen(8080, () => console.log("server started at port 8080"));
